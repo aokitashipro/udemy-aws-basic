df4b7f3b629e6ecef7756e38bbf209720c0121cd
aokitashipro <webmocha2@gmail.com>
2022-07-07 09:31:57 +0900

    change dbpassword

--- section5_mysql/index.php
+++ section5_mysql/index.php
@@ -1,27 +1,5 @@
 <?php
-const DB_HOST = 'mysql:dbname=udemy_aws;host=127.0.0.1;charset=utf8';
-const DB_USER = 'aws_user';
-const DB_PASSWORD = 'password123';
-
-try{
-  $pdo = new PDO(DB_HOST, DB_USER, DB_PASSWORD, [
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //連想配列
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //例外
-    PDO::ATTR_EMULATE_PREPARES => false, //SQLインジェクション対策
-  ]);
-
-  if($pdo == null){
-    echo '接続に失敗';
-  } else {
-    $stmt = $pdo->prepare("select * from posts");
-    $stmt->execute();
-    $posts = $stmt->fetchAll();
-  }
-} catch(PDOException $e){
-  echo '接続失敗' . $e->getMessage() . "\n";
-  exit();
-}
-
+require_once('db_connection.php');
 ?>
 <!DOCTYPE html>
 <html lang="ja">
