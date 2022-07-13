a32dd7f88fb2d9001bb347a106066be0013013a0
aokitashipro <webmocha2@gmail.com>
2022-07-13 15:39:34 +0900

    add iam role

--- section8_s3/bucketList.php
+++ section8_s3/bucketList.php
@@ -3,13 +3,27 @@ require './vendor/autoload.php';
 
 use Aws\S3\S3Client;
 
-$s3 = new S3Client([
+// IAMロールを使うため空設定
+$accessKeyId = '';
+$secretAccessKey = '';
+
+// S3インスタンス作成時の引数
+$config = [
     'version' => 'latest',
-    'region'  => 'ap-northeast-1'
-]);
+    'region'  => 'ap-northeast-1',
+    'profile' => 'default'
+];
+
+// AccessKeyとSecretAccressKeyがない場合は、IAMロールを使用する
+if (empty($awsAccessKeyId) && empty($awsSecretAccessKey)) {
+    // IAMロール認証の場合はprofileをセットしない(unsetする)
+    unset($config['profile']);
+  }
+
+$s3Client = new S3Client($config);
 
 # バケット一覧の表示
-$result = $s3->listBuckets();
+$result = $s3Client->listBuckets();
 foreach ($result['Buckets'] as $bucket) {
     echo $bucket['Name'] . "\n";
 }
